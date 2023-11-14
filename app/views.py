2d80f826ec1b20266d228e1f38d3af104c0992eb
veera4133 <katamveerasudheerkumar@gmail.com>
2023-11-14 16:49:17 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+def sudheer(request):
+    return render(request,'sudheer.html')
+
+
+def ruchi(request):
+    return render(request,'ruchi.html')
+
+
+def mini(request):
+    return render(request,'mini.html')
+
+
+def raju(request):
+    return render(request,'raju.html')
+
